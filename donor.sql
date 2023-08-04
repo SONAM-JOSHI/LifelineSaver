d27c73046ac4bd348414d71e58e71537fce4fd2b
Sonam Joshi <sonamjoshi@tuicms.edu.np>
2023-08-04 17:18:33 +0545

    added donor sql

---
+++ donor.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 04, 2023 at 01:29 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lifelinesaver`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donor`
+--
+
+CREATE TABLE `donor` (
+  `id` int(12) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `dob` varchar(20) NOT NULL,
+  `contact_no` varchar(16) NOT NULL,
+  `save_life_date` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `blood_group` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `donor`
+--
+
+INSERT INTO `donor` (`id`, `name`, `gender`, `email`, `city`, `dob`, `contact_no`, `save_life_date`, `password`, `blood_group`) VALUES
+(5, 'Ranchur Dash', 'Male', 'h2@gmail.com', 'Bhaktapur', '1972-01-14', '9812345675', '03/08/2023', 'Sonam123', 'O'),
+(8, 'Sonam Joshi', 'Female', 'sdsaf@gmail.com', 'Doti', '1972-08-15', '9812345675', '0', 'aada281e1cfa60ad1dde20134481f8ae', 'A-'),
+(10, 'Aryan Luitel', 'Male', 'aryan@gmail.com', 'Lalitpur', '1971-09-16', '9812312340', '0', 'ce2742720569b5498c40030d880f4c03', 'B+'),
+(11, 'Peayush Shrestha', 'Female', 'peayushstha@gmail.com', 'Itahari', '1975-08-17', '9812322332', '0', 'd26f386ab024f3494f1eac1d7f062d5a', 'A+');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `donor`
+--
+ALTER TABLE `donor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `donor`
+--
+ALTER TABLE `donor`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
